d6bc0adb67238e2affef4419d929a53139b4857f
Mattecoco <mmegalizzi@hotmail.fr>
2022-03-03 15:12:00 +0100

    Toastyyyy!
    + redirect sur nouvelle activité si appuie sur Entrées

--- app/src/main/java/fr/isen/megalizzi/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/megalizzi/androiderestaurant/HomeActivity.kt
@@ -1,11 +1,27 @@
 package fr.isen.megalizzi.androiderestaurant
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
+    }
+
+    fun displayToastMsg(v: View?) {
+        toastMsg("Ta grand mèèèère")
+    }
+
+    private fun toastMsg(msg: String?) {
+        val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    fun displayCategory(v: View?) {
+        setContentView(R.layout.activity_category)
     }
 }
