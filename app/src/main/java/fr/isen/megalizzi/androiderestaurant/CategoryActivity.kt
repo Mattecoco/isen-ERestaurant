840decc8a381922d14f3ea14b080ce8f9b075173
Mattecoco <mmegalizzi@hotmail.fr>
2022-03-03 16:26:26 +0100

    changement activité avec passage de titre en paramètre

--- app/src/main/java/fr/isen/megalizzi/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/megalizzi/androiderestaurant/CategoryActivity.kt
@@ -2,11 +2,15 @@ package fr.isen.megalizzi.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+
+        val title = findViewById<TextView>(R.id.categoryTitle)
+        title.text = intent.getStringExtra(HomeActivity.CATEGORY_KEY)
     }
 
 
--- app/src/main/java/fr/isen/megalizzi/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/megalizzi/androiderestaurant/HomeActivity.kt
@@ -1,19 +1,50 @@
 package fr.isen.megalizzi.androiderestaurant
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btn_entrees = findViewById<Button>(R.id.btnEntrees)
+        val btn_plats = findViewById<Button>(R.id.btnPlats)
+        val btn_desserts = findViewById<Button>(R.id.btnDesserts)
+
+        btn_entrees.setOnClickListener {
+            displayToastMsg()
+            displayCategory("Entrées")
+        }
+
+        btn_plats.setOnClickListener {
+            displayToastMsg()
+            displayCategory("Plats")
+        }
+
+        btn_desserts.setOnClickListener {
+            displayToastMsg()
+            displayCategory("Desserts")
+        }
+
     }
 
-    fun displayToastMsg(v: View?) {
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("HomeActivity", "L'activité HomeActivity est détruite.")
+    }
+
+
+    fun displayToastMsg() {
         toastMsg("Ta grand mèèèère")
     }
 
@@ -22,9 +53,14 @@ class HomeActivity : AppCompatActivity() {
         toast.show()
     }
 
-    fun displayCategory(v: View?) {
-        val categoryTitle = findViewById<TextView>(R.id.categoryTitle)
-        categoryTitle.text = "test"
-        setContentView(R.layout.activity_category)
+
+    fun displayCategory(category: String) {
+        val intent = Intent(this, CategoryActivity::class.java)
+        intent.putExtra(CATEGORY_KEY, category)
+        startActivity(intent)
+    }
+
+    companion object {
+        const val CATEGORY_KEY = "category"
     }
 }
