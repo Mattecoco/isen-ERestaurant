f2f46406b6c11c1352ef45c6b1e0cefbb076b5ee
Mattecoco <mmegalizzi@hotmail.fr>
2022-03-25 11:05:15 +0100

    terminé affichage du detail d'un plat, avec selection de la quantité + prix affiché

--- app/src/main/java/fr/isen/megalizzi/androiderestaurant/MealActivity.kt
+++ app/src/main/java/fr/isen/megalizzi/androiderestaurant/MealActivity.kt
@@ -2,12 +2,16 @@ package fr.isen.megalizzi.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Half.toFloat
+import android.view.View
 import android.widget.Toast
 import fr.isen.megalizzi.androiderestaurant.databinding.ActivityMealBinding
 
 class MealActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMealBinding
+    private var defaultPrice: Float? = null
+    private var quantity: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +34,41 @@ class MealActivity : AppCompatActivity() {
 
         binding.ingredients.text = textIngredients
 
+        /* set quantity to 1 */
+        binding.btnQuantity.text = quantity.toString()
+
+        /* set the default price */
+        defaultPrice = meal.prices[0].price?.toFloatOrNull()
+
+        if (defaultPrice != null)
+            binding.btnBuy.text = "TOTAL " + defaultPrice.toString().plus("€")
+        else
+            binding.btnBuy.text = "Error : no price"
+
         Toast.makeText(this, meal.nameFr, Toast.LENGTH_LONG).show()
+    }
+
+    private fun updateButtons() {
+        /* update the price */
+        val price = defaultPrice?.times(quantity)
+
+        /* update buttons Text */
+        binding.btnQuantity.text = quantity.toString()
+
+        if (price != null) {
+            binding.btnBuy.text = "TOTAL " + price.toString().plus("€")
+        }
+    }
+
+    fun minusOne(v :View?) {
+        if (this.quantity > 1) {
+            quantity.dec().also { quantity = it }
+            updateButtons()
+        }
+    }
+
+    fun plusOne(v: View?) {
+        quantity.inc().also { quantity = it }
+        updateButtons()
     }
 }
